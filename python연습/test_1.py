3cb99bd4fc7b829864a4a4bfaaebf376e8411014
J-Yeonju <Yeonju.dev@gmail.com>
2023-09-25 17:56:58 +0900

    study: python

---
+++ python연습/test_1.py
@@ -0,0 +1,20 @@
+# print ("Hi 나는 연주ㅎ")
+
+
+# print ("정수를 입력해주세요. 제곱을 알려드려요")
+# num = int(input())
+# print(num * num)
+
+
+# a, b = map(int, input().split())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+
+# print(input() + "??!")
+
+
+y = int(input())
+print(y-543)
